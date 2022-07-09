d7be43e275e6b51ca26e18b149006a6a75bf799c
kyleebm <kyleeboiceymacdonald@gmail.com>
2022-07-09 13:04:31 -0400

    made models

--- index.js
+++ index.js
@@ -8,16 +8,22 @@ const mongoose = require('mongoose')
 mongoose
   .connect('mongodb://localhost:27017/workoutAPI', { useNewUrlParser: true })
   .then(() => {
-    console.log(' MONGO CONNECTION OPEN!!')
+    console.log('MONGO CONNECTION OPEN!!')
   })
   .catch((err) => {
     console.log('OH NO MONGO CONNECTION ERROR', err)
   })
 
+//importing other files
+
+const User = require('./models/users')
+
+// routes
 app.get('/login', (req, res) => {
   res.send('hello')
 })
 
+//add port
 app.listen(3000, () => {
   console.log('APP IS LISTENING ON PORT 3000')
 })
