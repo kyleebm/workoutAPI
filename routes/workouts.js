fd9716d7f2be56876c8fc5ea1a9ed49ffd5299ac
kyleebm <kyleeboiceymacdonald@gmail.com>
2022-07-10 19:29:39 -0400

    made basic new route

--- routes/workouts.js
+++ routes/workouts.js
@@ -8,25 +8,24 @@ router.get('/workouts', (req, res) => {
   res.send('all workouts')
 })
 
+router.get('/workouts/new', (req, res) => {
+  res.send('a new workout')
+})
+
+router.post('/workouts/new', async (req, res) => {
+  const newWorkout = new Workout(req.body)
+  await newWorkout.save()
+  console.log(newWorkout)
+  res.send('made a new workout')
+  //res.redirect('/workouts')  go to the home page after saving
+})
+
 router.get('/workouts/:id', async (req, res) => {
   const { id } = req.params
   const workout = await Workout.findById(id)
   console.log(workout)
   res.send(workout)
   //res.redirect('/workout/:id)
-})
-
-router.post('/workouts/new', async (req, res) => {
-  const newWorkout = Workout(req.body)
-  await newWorkout
-    .save()
-    .then(
-      console.log(newWorkout)
-      //res.redirect('/dashboard') go to the home page after saving)
-    )
-    .catch((err) => {
-      console.log(err)
-    })
 })
 
 module.exports = router
