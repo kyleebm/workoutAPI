3b8a1918eff9359c053022ba7379d4e11b7b89e8
kyleebm <kyleeboiceymacdonald@gmail.com>
2022-07-10 20:30:33 -0400

    have basic crud

--- routes/workouts.js
+++ routes/workouts.js
@@ -25,7 +25,32 @@ router.get('/workouts/:id', async (req, res) => {
   const workout = await Workout.findById(id)
   console.log(workout)
   res.send(workout)
-  //res.redirect('/workout/:id)
+  //res.redirect(/workout/:id)
+})
+
+router.get('/workouts/:id/edit', async (req, res) => {
+  const { id } = req.params
+  const workout = await Workout.findById(id)
+  console.log(workout)
+  res.send(' edit post')
+})
+
+router.put('/workouts/:id', async (req, res) => {
+  const { id } = req.params
+  const newWorkout = req.body
+  const workout = await Workout.findByIdAndUpdate(id, newWorkout, {
+    runValidators: true,
+    new: true,
+  })
+  console.log(workout)
+  res.send('put request')
+})
+
+router.delete('/workouts/:id', async (req, res) => {
+  const { id } = req.params
+  const deletedWorkout = await Workout.findByIdAndDelete(id)
+  console.log(deletedWorkout)
+  res.send('delete route')
 })
 
 module.exports = router
