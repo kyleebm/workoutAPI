73415590d41526fc2bc91b93128ee16ca5a9fa13
kyleebm <kyleeboiceymacdonald@gmail.com>
2022-08-28 17:28:58 -0400

    set up fake user test

--- index.js
+++ index.js
@@ -47,6 +47,12 @@ app.use(passport.initialize())
 app.use(passport.session())
 passport.use(new LocalStrategy(User.authenticate()))
 
+app.get('/fakeUser', async (req, res) => {
+  const user = new User({ email: 'kyyyyy@gmail.com', username: 'kyyyyy' })
+  const newUser = await User.register(user, 'chicken')
+  res.send(newUser)
+})
+
 // routes
 const userRoutes = require('./routes/users')
 app.use('/register', userRoutes)
