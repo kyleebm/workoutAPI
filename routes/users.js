5464eb406bbd17efca61c87fa07221bbd44e567f
kyleebm <kyleeboiceymacdonald@gmail.com>
2022-12-29 17:21:07 -0500

    remove the user get routes

--- routes/users.js
+++ routes/users.js
@@ -5,15 +5,7 @@ const router = express.Router()
 //import models
 const { register,login } = require('../controllers/auth')
 
-router.get('/register', (req, res) => {
-  // res.send('register')
-  res.render('../views/users/register')
-})
 
-router.get('/login', (req, res) => {
-  // res.send('register')
-  res.render('../views/users/login')
-})
 
 
 
